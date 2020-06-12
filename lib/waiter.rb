8c1d7151d5a7ad88569130f1907cf72a696617bf
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-12 03:22:15 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,19 +1,33 @@
 class Waiter
-  attr_accessor :name, :yrs_experience
+  class Customer
+    attr_accessor :name, :age
 
-  @@all = []
-  def initialize(name, yrs_experience)
-    @name = name
-    @yrs_experience = yrs_experience
-    @@all << self
-  end
+    @@all = []
 
-  def self.all
-    @@all
-  end
+    def initialize(name, age)
+      @name = name
+      @age = age
+      @@all << self
+    end
 
-  def new_meal(customer, total, tip=0)
-    Meal.new(self, customer, total, tip)
-  end
+    def self.all
+      @@all
+    end
+
+    def meals
+      Meal.all.select do |meal|
+        meal.customer == self
+      end
+    end
+
+    def waiters
+      meals.map do |meal|
+        meal.waiter
+      end
+    end
+
+    def new_meal(waiter, total, tip=0)
+      Meal.new(waiter, self, total, tip)
+    end
 
 end
